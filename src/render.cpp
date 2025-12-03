04f342910d17b855a84c679228419fd768c3bdc0
RevelcoS <nertv04@mail.ru>
2025-12-03 16:45:14 +0300

    Get color with raycast

--- src/render.cpp
+++ src/render.cpp
@@ -27,17 +27,15 @@ namespace render {
 
     }
 
-    Color raycast(glm::vec3 origin, glm::vec3 direction);
-
+    Color get_color(glm::ivec2 coord);
     void render() {
 
         for (int i = 0; i < height; i++) {
 
             for (int j = 0; j < width; j++) {
 
-                glm::vec3 origin(0.0f);
-                glm::vec3 direction(1.0f);
-                Color color = render::raycast(origin, direction);
+                glm::ivec2 coord(i, j);
+                Color color = render::get_color(coord);
 
                 int offset = (i * width + j) * channels;
                 render::image[offset + 0] = color.r; 
@@ -61,6 +59,35 @@ namespace render {
 
 
 namespace render {
+
+    Color raycast(glm::vec3 origin, glm::vec3 direction);
+    Color get_color(glm::ivec2 coord) {
+
+        const float aspect_ratio = (float)width / height;
+        const float camera_width = 100.0f;
+        const float camera_height = camera_width / aspect_ratio;
+
+        // Camera top left and bottom right position
+        const glm::vec2 s1( -camera_width / 2,  camera_height / 2);
+        const glm::vec2 s2(  camera_width / 2, -camera_height / 2);
+
+        // Screen space uv coords
+        const glm::vec2 pixel_size(1.0f / width, 1.0f / height);
+        glm::vec2 uv = (glm::vec2(coord) + 0.5f) * pixel_size;
+
+        // World space ray coords
+        glm::vec2 ray = glm::mix(s1, s2, uv);
+    
+        // Ray origin and direction
+        glm::vec3 direction( ray.x, ray.y, -1.0f );
+        glm::vec3 origin(0.0f);
+
+        // Raycast
+        Color color = render::raycast(origin, direction);
+
+        return color;
+
+    }
 
     Color raycast(glm::vec3 origin, glm::vec3 direction) {
 
